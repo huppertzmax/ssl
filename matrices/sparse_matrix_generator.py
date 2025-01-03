a064d8fd08ffad6a004c2518bf8240c0013c03cb
Max Huppertz <max.huppertz@tum.de>
2025-01-03 16:29:18 +0100

    chore: file structure

---
+++ matrices/sparse_matrix_generator.py
@@ -0,0 +1,17 @@
+import time
+import numpy as np
+from scipy.sparse import random, save_npz
+
+n = 100_000  
+n_formatted = formatted = f"{n:,}".replace(",", "_")
+density = 2. / n
+
+start_time = time.time()
+rng = np.random.default_rng(seed=42) 
+sparse_matrix = random(n, n, density=density, format='csr', data_rvs=lambda size: rng.integers(0, 2, size=size), random_state=rng)
+
+end_time = time.time()
+print(f"Runtime for matrix generation: {end_time - start_time:.6f} seconds")
+
+save_npz(f"generated/sparse_matrix_{n_formatted}/matrix.npz", sparse_matrix)
+print(f"Matrix saved under: generated/sparse_matrix_{n_formatted}/matrix.npz")
