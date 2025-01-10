3b0a172b32e7b0db75dc0fad0e9f9e6fc0057550
Max Huppertz <max.huppertz@tum.de>
2025-01-10 19:23:29 +0100

    refactor: matrix generation logging and values

--- matrices/sparse_block_matrix_generator.py
+++ matrices/sparse_block_matrix_generator.py
@@ -18,8 +18,8 @@ def create_block_sparse_matrix(n_blocks, value=1.):
 
     return sparse_matrix.tocsr()
 
-num_samples = 2048
-n = num_samples * 10 * 2 * 50  
+num_samples = 4096
+n = num_samples * 10 * 2 * 25  
 n_formatted = formatted = f"{n:,}".replace(",", "_")
 n_blocks = n // 2
 value = 1./n
@@ -32,7 +32,7 @@ end_time = time.time()
 print(f"Runtime for matrix generation: {end_time - start_time:.6f} seconds")
 print(f"Matrix shape: {sparse_matrix.shape}")
 print(f"Matrix sum values: {sparse_matrix.sum()}\n")
-save_npz(f"./generated/sparse_matrix_{n_formatted}/block/matrix.npz", sparse_matrix)
+save_npz(f"./generated/sparse_matrix_{n_formatted}/block/matrix_n_{num_samples}.npz", sparse_matrix)
 
 start_time = time.time()
 row_sums = sparse_matrix.sum(axis=1).A1
@@ -40,13 +40,13 @@ diagonal_inv_sqrt_matrix = diags(1.0 / np.sqrt(row_sums), format="csr")
 end_time = time.time()
 print(f"Runtime for diagonal matrix generation: {end_time - start_time:.6f} seconds")
 print(f"Diagonal inverted square root matrix shape: {diagonal_inv_sqrt_matrix.shape}\n")
-save_npz(f"./generated/sparse_matrix_{n_formatted}/block/diagonal_inv_sqrt_matrix.npz", diagonal_inv_sqrt_matrix)
+save_npz(f"./generated/sparse_matrix_{n_formatted}/block/diagonal_inv_sqrt_matrix_n_{num_samples}.npz", diagonal_inv_sqrt_matrix)
 
 start_time = time.time()
 normalized_matrix = diagonal_inv_sqrt_matrix @ sparse_matrix @ diagonal_inv_sqrt_matrix
 end_time = time.time()
 print(f"Runtime for normalized matrix generation: {end_time - start_time:.6f} seconds")
 print(f"Normalized matrix shape: {normalized_matrix.shape}\n")
-save_npz(f"./generated/sparse_matrix_{n_formatted}/block/normalized_matrix.npz", normalized_matrix)
+save_npz(f"./generated/sparse_matrix_{n_formatted}/block/normalized_matrix_n_{num_samples}.npz", normalized_matrix)
 
 print(f"Matrices saved under: generated/sparse_matrix_{n_formatted}/block")
