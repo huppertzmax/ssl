1ac2f456f886d018f65abdb60ee9b87e75d8c6ea
Max Huppertz <max.huppertz@tum.de>
2025-01-13 11:23:30 +0100

    feat: chunked mnist dataloader, model summary, wandb changes

--- training/tiny_pretraining.py
+++ training/tiny_pretraining.py
@@ -4,12 +4,14 @@ import numpy
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import LearningRateMonitor
+from torchsummary import summary
 
 from training.utils.model_checkpoint import ModelCheckpoint
 from training.models.tiny_extractor import TinyExtractor
 from training.models.tiny_mnist_extractor import TinyMNISTExtractor
 from dataset.tiny_cifar10 import TinyCIFAR10DataModule
 from dataset.tiny_mnist import TinyMNISTDataModule
+from dataset.tiny_chunked_mnist import TinyChunkedMNISTDataModule
 from training.utils.utils import log_msg
 
 
@@ -17,7 +19,7 @@ def tiny_pretraining(config, args, isTune=False):
     args.__dict__.update(config)
     
     if not isTune and args.fast_dev_run == 0:
-        wandb_logger = WandbLogger(project='SSL')
+        wandb_logger = WandbLogger(project='SSL', tags=['pretraining', 'tiny'])
     log_msg(args)
 
     if args.dataset == "cifar10": 
@@ -25,9 +27,14 @@ def tiny_pretraining(config, args, isTune=False):
         args.num_samples = dm.num_samples
         model = TinyExtractor(**args.__dict__)
     elif args.dataset == "mnist":
-        dm = TinyMNISTDataModule(data_dir=args.data_dir, train_subset_name=args.train_subset_name, val_subset_name=args.val_subset_name, 
-                                 batch_size=args.batch_size, num_workers=args.num_workers)
+        if args.chunked_data: 
+            dm = TinyChunkedMNISTDataModule(data_dir=args.data_dir, train_subset=args.train_subset_name, 
+                    val_subset=args.val_subset_name, batch_size=args.batch_size, num_workers=args.num_workers)
+        else: 
+            dm = TinyMNISTDataModule(data_dir=args.data_dir, train_subset_name=args.train_subset_name,
+                    val_subset_name=args.val_subset_name, batch_size=args.batch_size, num_workers=args.num_workers)
         model = TinyMNISTExtractor(**args.__dict__)
+        summary(model.cuda(), (1, 28, 28))
     else:
         raise NotImplementedError("other datasets have not been implemented till now")
     
@@ -47,6 +54,7 @@ def tiny_pretraining(config, args, isTune=False):
         "precision": 32 if args.fp32 else 16,
         "callbacks": callbacks,
         "fast_dev_run": args.fast_dev_run,
+        "val_check_interval": 0.1 if args.max_epochs == 1 else 1.0
     }
 
     if not isTune and args.fast_dev_run == 0:
@@ -57,4 +65,6 @@ def tiny_pretraining(config, args, isTune=False):
     trainer.fit(model, datamodule=dm)
 
     if not isTune and args.fast_dev_run == 0: 
+        run_name = wandb.run.name
         wandb.finish()
+        return run_name
